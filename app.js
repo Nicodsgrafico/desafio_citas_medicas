bb8fe72a376d3133d054a2254e3b570ecd9fdd86
Nicolas Espinoza <nico.dsgrafico@gmail.com>
2024-05-08 12:21:21 -0400

    :zap: Se modifican rutas y se genera lista de usuarios al registrar cada uno

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
 import _ from 'lodash';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 let usuarios = [];
+const __dirname = path.resolve();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
 
+app.use(/bootstrap/, express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 
 app.get('/', (req, res) => {
-    res.send('Hola, para registrar un usuario use la ruta /registrar o haga click aqui: <a href="/registrar">Registrar</a>');   
+    res.send('Hola, para registrar un usuario use la ruta /registrar o haga click aqui: <a href="/registrar">Registrar</a>');
 })
 //Ruta para registrar un usuario
 app.get('/registrar', (req, res) => {
@@ -30,21 +33,47 @@ app.get('/registrar', (req, res) => {
         //Se crea timestamp con moment
         const timestamp = moment().locale('es').format('MMMM Do YYYY, h:mm:ss a');
 
-         const usuarioregistrado = {
+        //Se crea objeto con los datos del usuario
+        const usuarioregistrado = {
             firstName: userData.name.first,
             lastName: userData.name.last,
+            gender: userData.gender,
             id: id,
-            timestamp : timestamp,
+            timestamp: timestamp,
         }
+        //Se agrega el usuario a la lista de usuarios
         usuarios.push(usuarioregistrado);
-        
-//Si no se logra obtener datos del usuario se redirecciona a la ruta principal 
-}).catch((error) => {
-    console.log(error);
-    res.redirect('/');
-})
+        //Se redirecciona a la ruta de usuarios
+        res.redirect('/usuarios');
+
+        //Si no se logra obtener datos del usuario se redirecciona a la ruta principal 
+    }).catch((error) => {
+        console.log(error);
+        res.redirect('/');
+    })
 })
 
+// Ruta para consultar usuarios
 app.get('/usuarios', (req, res) => {
-    res.send(usuarios);
-})
+    // Separar los usuarios por genero
+const [hombres, mujeres] = _.partition(usuarios, { gender: 'male' });
+// Función para generar una lista HTML de usuarios
+function generarListaUsuarios(usuarios) {
+    return `
+<ul style="list-style: none;">
+    ${usuarios.map(usuario =>  `<li class="list-group-item">Nombre: ${usuario.firstName} Apellido: ${usuario.lastName} ID: ${usuario.id} Timestamp: ${usuario.timestamp}</li>`).join('\n')}
+</ul>
+`;
+}
+
+// Generar la respuesta HTML con las listas de hombres y mujeres
+const respuestaHTML = `
+<h2>Hombres</h2>
+${generarListaUsuarios(hombres)}
+<h2>Mujeres</h2>
+${generarListaUsuarios(mujeres)}
+`;
+
+// Enviar la respuesta HTML al cliente
+res.send(respuestaHTML+'Registrar nuevo usuario: <a href="/registrar">Registrar</a>');
+});
